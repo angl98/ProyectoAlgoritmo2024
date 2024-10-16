9df499960de0b0a303331fa1a41b80699fc5b012
angl98 <angelmayen11@gmail.com>
2024-10-15 18:01:28 -0600

    NuevoProyecto en java por erro de classpath

---
+++ ProyectoAlgoritmo/src/main/java/com/mycompany/proyectoalgoritmo/ProyectoAlgoritmo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoalgoritmo;
+
+/**
+ *
+ * @author angel
+ */
+public class ProyectoAlgoritmo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
